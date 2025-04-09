980063e537b1028f8c86e45c341fed5ac1f4c515
SantiGomez2519 <sgomezo14@eafit.edu.co>
2025-04-09 13:05:44 -0500

    Fix - Navbars

--- ad/urls.py
+++ ad/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import create_campaign, campaign_list, campaign_success, schedule_ad
+from .views import create_campaign, campaign_list, campaign_success
 
 urlpatterns = [
     path("create/", create_campaign, name="create_campaign"),
     path("campaigns/", campaign_list, name="campaign_list"),
     path("success/",campaign_success, name="campaign_success"),
-    path("schedule/", schedule_ad, name="schedule_ad")
 ]
