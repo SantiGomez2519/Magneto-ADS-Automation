ec3bf8b85afe85efda89a9ca96bc81a8f3d83e67
SantiGomez2519 <sgomezo14@eafit.edu.co>
2025-04-09 12:37:54 -0500

    Feature - Schedule Ad

--- ad/urls.py
+++ ad/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import create_campaign, campaign_list, campaign_success
+from .views import create_campaign, campaign_list, campaign_success, schedule_ad
 
 urlpatterns = [
     path("create/", create_campaign, name="create_campaign"),
     path("campaigns/", campaign_list, name="campaign_list"),
     path("success/",campaign_success, name="campaign_success"),
+    path("schedule/", schedule_ad, name="schedule_ad")
 ]
