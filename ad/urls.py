139eb999ea3774f8ad9db8910eedcdb3be819e1a
SantiGomez2519 <sgomezo14@eafit.edu.co>
2025-05-20 23:12:28 -0500

    Feature: Simulation of metrics for campaigns

--- ad/urls.py
+++ ad/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import create_campaign, campaign_list, campaign_success, add, edit_campaign, delete_campaign, platforms_list, ad_preview
+from .views import create_campaign, campaign_list, campaign_success, add, edit_campaign, delete_campaign, platforms_list, ad_preview, simulate_metrics_view, performance_metrics_view, simulate_platform_metrics
 
 urlpatterns = [
     path("create/", create_campaign, name="create_campaign"),
@@ -9,5 +9,8 @@ urlpatterns = [
     path('edit/<int:campaign_id>/', edit_campaign, name='edit_campaign'),
     path('delete/<int:campaign_id>/', delete_campaign, name='delete_campaign'),
     path('platforms/', platforms_list, name='platforms_list'),
+    path('platforms/<int:campaign_id>/simulate/<str:platform>/', simulate_platform_metrics, name='simulate_platform_metrics'),
     path('preview/<int:campaign_id>/<str:platform>/', ad_preview, name='ad_preview'),
+    path('simulate-metrics/<int:campaign_id>/', simulate_metrics_view, name='simulate_metrics'),
+    path('performance-metrics/', performance_metrics_view, name='performance_metrics'),
 ]
